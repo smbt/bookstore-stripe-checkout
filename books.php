aa89866b2f896bacb1d09b824853bfe9ff5836ae
Simon <simon_brandt@gmx.de>
2019-11-19 15:31:16 +0100

    ADD book images, USE them

--- books.php
+++ books.php
@@ -3,7 +3,7 @@ $books = [
     [
         'name' => 'PHP For Beginners',
         'description' => 'Learn PHP from the ground up.',
-        'images' => ['https://thumbs-prod.si-cdn.com/T_txo7Wkgu6aPjd6a8thp1kDL2s=/420x240/https://public-media.si-cdn.com/filer/91/91/91910c23-cae4-46f8-b7c9-e2b22b8c1710/lostbook.jpg', 'https://mgbookvillage.files.wordpress.com/2017/11/cropped-book1.jpg'],
+        'images' => [getenv('BASE_URL') . 'images/php.jpg'],
         'amount' => 4999,
         'currency' => 'eur',
         'quantity' => 1,
@@ -11,7 +11,7 @@ $books = [
     [
         'name' => 'JavaScript For Beginners',
         'description' => 'Learn PHP from the ground up.',
-        'images' => ['https://thumbs-prod.si-cdn.com/T_txo7Wkgu6aPjd6a8thp1kDL2s=/420x240/https://public-media.si-cdn.com/filer/91/91/91910c23-cae4-46f8-b7c9-e2b22b8c1710/lostbook.jpg', 'https://mgbookvillage.files.wordpress.com/2017/11/cropped-book1.jpg'],
+        'images' => [getenv('BASE_URL') . 'images/js.jpg'],
         'amount' => 3999,
         'currency' => 'eur',
         'quantity' => 1,
